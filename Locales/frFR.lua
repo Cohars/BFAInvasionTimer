9cd2a4cad672ef67128dcb50e7766ac4598edd7b
Cohars <cohars@free.fr>
2019-02-25 11:02:09 +0100

    Update frFR.lua

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "frFR" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T%d:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
---L.nextInvasions = "Next Invasions"
---L.next = "Next: %s"
---L.waiting = "Waiting"
+--L.firstRunWarning = "Les timers serront affichées quand vous aurez vu la première invasion."
+--L.underAttack = "|T%d:15:15:0:0:64:64:4:60:4:60|t %s est actuellement attaqué!"
+--L.nextInvasions = "Prochaines Invasions"
+--L.next = "Prochaine: %s"
+--L.waiting = "En attente"
